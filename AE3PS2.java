d6429558507460d1403a2b7339555d011b379c0d
Marina Schramm Jorge Schneider <84358232+marinasjs@users.noreply.github.com>
2021-12-07 10:22:18 -0300

    Add files via upload

---
+++ AE3PS2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ae3ps2;
+
+/**
+ *
+ * @author marina
+ */
+public class AE3PS2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TelaAvaliacao tc = new TelaAvaliacao();
+        tc.setVisible(true);
+    }
+    
+}
